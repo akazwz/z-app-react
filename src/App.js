e5b74aa2dc997e5081fedd54fc830e5daa75fbaf
akez <1791189210@qq.com>
2021-06-02 12:02:31 +0800

    fileInput

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
+import FileInput from "./components/FileInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <FileInput/>
+    )
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>
+}
+
+function About() {
+    return <h2>About</h2>
+}
+
+function Topics() {
+    let match = useRouteMatch();
+
+    return (
+        <div>
+            <h2>Topics</h2>
+
+            <ul>
+                <li>
+                    <NavLink to={`${match.url}/components`}>Components</NavLink>
+                </li>
+                <li>
+                    <NavLink to={`${match.url}/props-v-state`}>
+                        Props v. State
+                    </NavLink>
+                </li>
+            </ul>
+
+            <Switch>
+                <Route path={`${match.path}/:topicId`}>
+                    <Topic/>
+                </Route>
+                <Route path={match.path}>
+                    <h3>Please select a topic.</h3>
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+function Topic() {
+    let {topicId} = useParams();
+    return <h3>Request topic ID: {topicId}</h3>;
+}
---
+++ src/components/FileInput.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class FileInput extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fileInput = React.createRef();
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(
+            `Selected file - ${this.fileInput.current.files[0].name}`
+        );
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Upload File:
+                    <input type="file" ref={this.fileInput}/>
+                </label>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default FileInput
